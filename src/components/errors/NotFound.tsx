78ffaaccadecbdd78e8129354b62ab6414419bbf
Rasel Developer <dev.code.get@gmail.com>
2023-09-16 21:01:52 +0600

    new one

--- src/components/errors/NotFound.tsx
+++ src/components/errors/NotFound.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { FiCloudOff } from 'react-icons/fi';
 import { HiOutlineEmojiSad } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
@@ -22,37 +20,6 @@ export const Unauthorized = () => (
 				</p>
 				<p className='mb-2 text-3xl font-bold text-gray-200'>Unauthorized</p>
 				<p className='text-gray-300'>Sorry! you can not access this page :(</p>
-				<GoHome />
-			</div>
-		</main>
-	</>
-);
-
-type Props = {
-	label: string;
-	children: React.ReactNode;
-};
-
-export const NoData: React.FC<Props> = ({ label, children }) => (
-	<div className='py-5'>
-		<div className='flex flex-col items-center'>
-			<span className='mb-3 text-cst-secondary'>{children}</span>
-			<span className='text-cst-secondary-light'>{label}</span>
-		</div>
-	</div>
-);
-
-export const GameNotFound = () => (
-	<>
-		{/* <div className='p-3' /> */}
-		<main className='flex-1 page_body pb-3'>
-			<div className='mx-3 md:mx-0 select-none overflow-hidden'>
-				<p className='mb-2 text-5xl text-cst-secondary-light'>
-					<FiCloudOff />
-				</p>
-				<p className='flex items-center text-lg font-bold text-cst-secondary-light'>
-					This match isn&apos;t available <br /> at the moment!
-				</p>
 				<GoHome />
 			</div>
 		</main>
